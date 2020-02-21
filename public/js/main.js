6699b761c9230672b7660aab49a3eba12bb9ea2b
Nikhil <nikhilvarghese@tutamail.com>
2020-02-21 18:12:16 +0530

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,146 @@
+(function($) {
+	"use strict"
+
+// For client scroller
+// $(document).ready(function(){
+//     $('.customer-logos').slick({
+//         slidesToShow: 8,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 1500,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: false,
+//         responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }, {
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }]
+//     });
+// });
+
+
+	///////////////////////////
+	// Preloader
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
+
+	///////////////////////////
+	// Scrollspy
+	$('body').scrollspy({
+		target: '#nav',
+		offset: $(window).height() / 2
+	});
+
+	///////////////////////////
+	// Smooth scroll
+	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+	$('#back-to-top').on('click', function(){
+		$('body,html').animate({
+			scrollTop: 0
+		}, 600);
+	});
+
+	///////////////////////////
+	// Btn nav collapse
+	$('#nav .nav-collapse').on('click', function() {
+		$('#nav').toggleClass('open');
+	});
+
+	///////////////////////////
+	// Mobile dropdown
+	$('.has-dropdown a').on('click', function() {
+		$(this).parent().toggleClass('open-drop');
+	});
+
+	///////////////////////////
+	// On Scroll
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+
+		// Fixed nav
+		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
+
+		// Back To Top Appear
+		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
+	});
+
+	///////////////////////////
+	// magnificPopup
+	// $('.work').magnificPopup({
+	// 	delegate: '.lightbox',
+	// 	type: 'image'
+	// });
+
+	///////////////////////////
+	// Owl Carousel
+
+
+	// $('#testimonial-slider').owlCarousel({
+	// 	loop:true,
+	// 	margin:15,
+	// 	dots : true,
+	// 	nav: false,
+	// 	autoplay : true,
+	// 	responsive:{
+	// 		0: {
+	// 			items:1
+	// 		},
+	// 		992:{
+	// 			items:2
+	// 		}
+	// 	}
+	// });
+
+
+	// TODO commented to resolve owl carousel error
+	// $('#about-slider').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:15,
+	// 	nav: true,
+	// 	navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	dots : true,
+	// 	autoplay : true,
+	// 	animateOut: 'fadeOut'
+	// });
+
+	// $('#about-slider2').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:15,
+	// 	nav: true,
+	// 	navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	dots : true,
+	// 	autoplay : true,
+	// 	animateOut: 'fadeOut'
+	// });
+
+	// $('#about-slider3').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:15,
+	// 	nav: true,
+	// 	navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	dots : true,
+	// 	autoplay : true,
+	// 	animateOut: 'fadeOut'
+	// });
+
+	
+
+})(jQuery);
